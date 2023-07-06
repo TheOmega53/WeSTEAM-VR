c0d4def28cd8318b2f2dfdb802965238eed71ca3
kamyab78 <amirhosseinghassemi78@yahoo.com>
2023-07-06 08:34:55 -0700

    Add menu and render object

--- Assets/whiteBoard/script/ExportToObject.cs
+++ Assets/whiteBoard/script/ExportToObject.cs
@@ -5,36 +5,45 @@ using UnityEngine;
 public class ExportToObject : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    
     public GameObject sourceObject;
-    public GameObject destinationObject;
+    public GameObject cubeObject;
+    public GameObject cylinderObject;
+
     void Start()
     {
+        Renderer cubeRenderer = cubeObject.GetComponent<Renderer>();
+        cubeRenderer.enabled = false;
         
+        Renderer cylinderRenderer = cylinderObject.GetComponent<Renderer>();
+        cylinderRenderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ExportAndAssignTexture();
+        // ExportAndAssignTexture();
     }
-    public void ExportAndAssignTexture()
-    {
-        // Code for exporting and assigning the texture
+    
+    public void ExportAndAssignTextureCube()
+    { 
         Renderer sourceRenderer = sourceObject.GetComponent<Renderer>();
-    
-        // Get the material of the source renderer
         Material sourceMaterial = sourceRenderer.material;
-    
-        // Get the texture from the source material
         Texture sourceTexture = sourceMaterial.mainTexture;
-    
-        // Get the renderer component of the destination object
-        Renderer destinationRenderer = destinationObject.GetComponent<Renderer>();
-    
-        // Get the material of the destination renderer
-        Material destinationMaterial = destinationRenderer.material;
-    
-        // Assign the texture to the destination material
-        destinationMaterial.mainTexture = sourceTexture;
+        Renderer cubeRenderer = cubeObject.GetComponent<Renderer>();
+        cubeRenderer.enabled = true;
+        Material cubeMaterial = cubeRenderer.material;
+        cubeMaterial.mainTexture = sourceTexture;
+    }
+    public void ExportAndAssignTextureCylinder()
+    { 
+        Renderer sourceRenderer = sourceObject.GetComponent<Renderer>();
+        Material sourceMaterial = sourceRenderer.material;
+        Texture sourceTexture = sourceMaterial.mainTexture;
+        Renderer cylinderRenderer = cylinderObject.GetComponent<Renderer>();
+        cylinderRenderer.enabled = true;
+        Material cylinderMaterial = cylinderRenderer.material;
+        cylinderMaterial.mainTexture = sourceTexture;
     }
 }
